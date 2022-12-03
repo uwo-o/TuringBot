9c07ad0dc981e417f2ab6a4f0e987454ff781362
uwo-o <hugo.campos@usm.cl>
2022-12-03 17:41:31 -0300

    The basic idea of logic is planted

--- responses.py
+++ responses.py
@@ -1,2 +1,19 @@
-def handle_message(message) -> str:
-    return "uwu"
+'''
+This dictionary has the avalaibles commands as key and an integer
+as value, the value represent:
+1: Admin command
+0: Members command
+'''
+command_list = {
+    "crear_evento": 1,
+    "ver_eventos": 1,
+    "entrar_evento": 0,
+    "ayuda": 0
+}
+
+def handle_message(message:str):
+    
+    command = message.split(" ")[0]
+    
+    if command not in command_list:
+        return f"El comando '{command}' no existe. Para conocer los comandos utilice >ayuda"
