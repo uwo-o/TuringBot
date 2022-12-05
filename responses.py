18298ebc965738dd748d685320603c70c7a1c9db
Hugo <hugo.campos@usm.cl>
2022-12-05 04:44:58 -0300

    new_ask function implemented

--- responses.py
+++ responses.py
@@ -1,3 +1,25 @@
+import configparser
+import os
+
+def new_ask(message):
+
+    config_parser = configparser.ConfigParser.read(open(os.path.dirname(os.path.abspath(__file__)+".conf")))
+
+    questions_channel = config_parser.get("GLOBAL", "QUESTIONS_CHANNEL")
+
+    if message.channel != questions_channel:
+        return "Solo es posible crear preguntas dentro del canal: **"+questions_channel+"**"
+
+    question = " ".join(message.split()[1:])
+    return {
+        "status": 0,
+        "created_at": message.created_at,
+        "user": message.display_name,
+        "question": question,
+        "answered_by": ""
+    }
+
+
 '''
 This dictionary has the avalaibles commands as key and an integer
 as value, the value represent:
@@ -8,6 +30,7 @@ command_list = {
     "crear_evento": 1,
     "ver_eventos": 1,
     "entrar_evento": 0,
+    "pregunta": new_ask,
     "ayuda": 0
 }
 
@@ -17,3 +40,5 @@ def handle_message(message:str):
     
     if command not in command_list:
         return f"El comando '{command}' no existe. Para conocer los comandos utilice >ayuda"
+
+    return command_list[command](message)
