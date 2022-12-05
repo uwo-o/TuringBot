a97df66036397202a9fd97c7f4f0e392e39a4465
Hugo <hugo.campos@usm.cl>
2022-12-05 12:19:00 -0300

    Commented functions and correted old comments

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 import os
 
+# Channels configuration
 QUESTIONS_CHANNEL = "turing-bot"
 
+# Path constants
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 QUESTIONS_PATH = os.path.join(ROOT_PATH, "data/questions/")
 
+# If the reply type is sent privately or in the same channel
 message_privacy = {
     "question": False
 }
--- responses.py
+++ responses.py
@@ -3,15 +3,21 @@ import json
 import os
 
 def new_ask(message):
+    '''
+    This function generate a JSON file to storage the data for each question,
+    it is only to prevent lost questions if some problem appear
+    '''
 
     questions_channel = config.QUESTIONS_CHANNEL
 
+    # If the message channel is not the designe to questions, returns error.
     if str(message.channel) != questions_channel:
         return {
             "id": "question",
             "message": f"Solo es posible crear preguntas dentro del canal: **{questions_channel}**"
             }
 
+    # If the question doesn't contain text, returns error.
     if len(str(message.content).split()) == 1:
         return {
             "id": "question",
@@ -30,8 +36,10 @@ def new_ask(message):
             "answered_by": ""
         }, file)
 
+    # It counts how many questions there are to set a position in the line.
     position = len([file for file in os.listdir(config.QUESTIONS_PATH) if os.path.isfile(config.QUESTIONS_PATH+file)])
 
+    # Feedback
     return {
         "id": "question",
         "message": f"**[Turno: {str(position)}] {str(message.author.display_name)}** tu pregunta ha sido creada exitosamente!"
@@ -39,17 +47,11 @@ def new_ask(message):
 
 
 '''
-This dictionary has the avalaibles commands as key and an integer
-as value, the value represent:
-1: Admin command
-0: Members command
+This dictionary has the avalaibles commands as key and their
+respective function as value.
 '''
 command_list = {
-    "crear_evento": 1,
-    "ver_eventos": 1,
-    "entrar_evento": 0,
-    "pregunta": new_ask,
-    "ayuda": 0
+    "pregunta": new_ask
 }
 
 def handle_message(message, user_message:str):
