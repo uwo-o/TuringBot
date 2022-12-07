6cad772f98a4dac9a992495bd68c021aa3766487
uwo-o <hugo.campos@usm.cl>
2022-12-07 00:53:50 -0300

    new structure

--- config.py
+++ config.py
@@ -5,7 +5,16 @@ QUESTIONS_CHANNEL = "turing-bot"
 
 # Path constants
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
-QUESTIONS_PATH = os.path.join(ROOT_PATH, "data/questions/")
+DATA_PATH = os.path.join(ROOT_PATH, "data")
+QUESTIONS_PATH = os.path.join(ROOT_PATH, "data\\questions\\")
+
+GUILD_FOLDERS_SYSTEM = [
+    ["questions", 
+        [
+            ["answered", []]
+        ]
+    ]
+]
 
 # If the reply type is sent privately or in the same channel
 message_privacy = {
