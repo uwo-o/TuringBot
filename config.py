681c747b349ffda8ddafdce35dc1dd37c7b23fdf
uwo-o <hugo.campos@usm.cl>
2023-01-02 01:33:52 -0300

    commands tree added

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+from commands import configuration, help, questions
 import os
 
 # Channels configuration
@@ -6,7 +7,6 @@ QUESTIONS_CHANNEL = "turing-bot"
 # Path constants
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 DATA_PATH = os.path.join(ROOT_PATH, "data")
-QUESTIONS_PATH = os.path.join(ROOT_PATH, "data\\questions\\")
 
 GUILD_FOLDERS_SYSTEM = [
     ["questions", 
@@ -22,3 +22,30 @@ message_privacy = {
     "help": False,
     "config": False
 }
+
+# The command tree, the tuples follows the next order: (ADMIN PRIVILEGES, ASSISTAND PRIVILEGES, FUNCTION)
+COMMANDS = {
+    "ayuda": (0, 0, help.help),
+    "pregunta": (0, 0, questions.new_ask),
+    "config": {
+        "canales": {
+            "cambiar_preguntas": None, #To implement
+            "default_preguntas": None #To implement
+        },
+        "roles": {
+            "ver": (1, 0, configuration.get_roles),
+            "seleccionar": {
+                "admin": None, #To implement
+                "ayudantes": None, #To implement
+                "usuarios": None, #To implement
+            },
+            "borrar": {
+                "admin": None, #To implement
+                "ayudantes": None, #To implement
+                "usuarios": None, #To implement
+            }
+        }
+    }
+}
+
+NOT_FOUND_COMMAND = (0, 0, help.not_exist)
